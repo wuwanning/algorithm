12eb6b0439f6db04df1dcbd0f957047865744a3c
miku <10730429543@qq.com>
2019-07-10 11:10:51 +0800

    数组 文件化存储和读取

--- src/main/java/cn/mikulove/datastructure/sparearray/SparseArray.java
+++ src/main/java/cn/mikulove/datastructure/sparearray/SparseArray.java
@@ -1,18 +1,29 @@
 package cn.mikulove.datastructure.sparearray;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class SparseArray {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int[][] array = new int[9][9];
 		array[3][4] = 1;
 		array[4][4] = 2;
 		array[4][3] = 1;
 		
-		SparseArray.printArray(array);
+		/*SparseArray.printArray(array);
 		
 		SparseArray.printArray(SparseArray.normalToSparse(array));
 		
 		SparseArray.printArray(SparseArray.sparseToNormal(SparseArray.normalToSparse(array)));
+	
+		SparseArray.arrayToFile(SparseArray.normalToSparse(array),new File("D:/map.data"));*/
+		
+		SparseArray.printArray(SparseArray.arrayToFile(new File("D:/map.data")));
 	}
 	
 	/**
@@ -87,6 +98,66 @@ public class SparseArray {
 			System.out.println();
 		}
 		
+	}
+	
+	/**
+	 * 将二维数组 转成 文件
+	 * @param array
+	 * @param file
+	 * @throws IOException 
+	 */
+	private static void arrayToFile(int[][] array,File file) throws IOException{
+		//如果文件不存在 新建文件
+		if(!file.exists())
+			file.createNewFile();
+		
+		//选择文件流
+		Writer fw = new FileWriter(file);
+		
+		//使用流写入文件
+		for(int[] arr:array){
+			for(int i:arr){
+				fw.write(i+"\t");
+			}
+			fw.write("\r\n");
+		}
+		//最后关闭文件流
+		fw.close();
+		
+	}
+	
+	
+	private static int[][] arrayToFile(File file) throws IOException{
+		//1.如果文件不存在 新建文件
+		if(!file.exists())
+			return null;
+	    //2.选择流
+	    BufferedReader in = new BufferedReader(new FileReader(file));
+	    //3.1进行数据的搬移，但是数组首要考虑的事情是数组要多大？
+	    int row =0;//用于创建要创建的二维稀疏数组的大小确定
+	    String line; //一行数据
+	    //逐行读取，并将每个数组放入到数组中
+	    while ((line = in.readLine()) != null) {
+	        row++;
+	    }
+	    //由于读取完毕整个文本文档，所以重启流
+	    in.close();
+	    int[][] sparseArr = new int [row][3];
+	    //3.2文本数据转移到稀疏数组中
+	    int rowtmp = 0;
+	    in = new BufferedReader(new FileReader(file));
+	    while ((line = in.readLine()) != null) {
+	        String[] temp = line.split("\t");
+	        for (int j = 0; j < temp.length; j++) {
+	        	sparseArr[rowtmp][j]=Integer.parseInt(temp[j]);
+	        }
+	        rowtmp++;
+	    }
+	    //4.关闭流
+	    in.close();
+
+
+		return sparseArr;
 	}
 
 }
